57d711fa442e0876efce4eced205e2e8239e4536
Gia Thanh <duonggiathanh3819@gmail.com>
2020-10-16 11:41:14 +0700

    Gia Thanh cau 10 Update

--- Source.cpp
+++ Source.cpp
@@ -13,7 +13,11 @@ int main()
 	
 	int tongab= tong(a,b);
 	
-	cout<<"tong a + b = "<<tong;
+	cout<<"tong a + b = "<<tongab;
+	
+	int hieuab = hieu(a,b);
+	cout <<"Hieu a - b = " << hieuab << endl;
+	
 	system("pause");
 	return 0;
 }
@@ -23,4 +27,9 @@ int tong(int a, int b)
 	int tong=0;
 	tong=a+b;
 	return tong;
+}
+int hieu(int a, int b){
+	int sum = 0;
+	sum = a - b;
+	return b;
 }
