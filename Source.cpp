9479553eb1929753bcde5603924f59098b58873e
Tan Phong <tanphong15987532@gmail.com>
2020-10-16 11:20:21 +0700

    cau10 nhomtruong

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,19 @@ float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a = 10;
+	int b = 2;
+	
+	int tongab= tong(a,b);
+	
+	cout<<"tong a + b = "<<tong;
 	system("pause");
 	return 0;
+}
+
+int tong(int a, int b)
+{
+	int tong=0;
+	tong=a+b;
+	return tong;
 }
